f05ef84c6a10d75c58197578816618945d37940f
lamafab <42901763+lamafab@users.noreply.github.com>
2021-02-28 00:15:30 +0100

    init indentity module and IdentityOfStore

---
+++ src/frame/identity.rs
@@ -0,0 +1,21 @@
+//! Implements support for the pallet_identity module.
+
+use super::balances::Balances;
+use codec::Encode;
+
+pub use pallet_identity::{
+    Registration,
+
+};
+
+/// The subset of the `frame::Trait` that a client must implement.
+#[module]
+pub trait Identity: Balances {}
+
+#[derive(Clone, Debug, Eq, PartialEq, Encode, Store)]
+/// Information that is pertinent to identify the entity behind an account.
+pub struct IdentityOfStore<T: Identity> {
+    #[store(returns = Option<Registration<T::Balance>>)]
+    /// Information concerning the identity of the controller of an account. 
+    pub registration: Registration<T::Balance>,
+}
